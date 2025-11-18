48612d4f50af59197371c0ee76dfbc9a6b5674b5
harsheeka <yoyoharshikaa@gmail.com>
2025-11-18 11:27:05 +0530

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import requests
 
-BACKEND_URL = "https://meal-planner-backend-8hs4.onrender.com"
+BACKEND_URL = "http://127.0.0.1:8000/mealplan"
 
 st.set_page_config(page_title="Weekly Meal Planner", layout="wide")
 st.title("🍛 Weekly Meal Planner")
